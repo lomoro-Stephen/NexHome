baa2b67617c109f5c298109060cf7f43d26ee95b
lomoro-Stephen <lomorostphn@gmail.com>
2024-11-20 11:36:46 +0300

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/about";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+
 
 export default function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/SignIn" element={<SignIn/>} />
+        <Route path="/SignOut" element={<SignUp/>} />
+        <Route path="/Profile" element={<Profile/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SignIn() {
+  return (
+    <div>
+      <h3>sign in</h3>
+    </div>
+  )
+}
